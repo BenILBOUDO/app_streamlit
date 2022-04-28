341263ce22a1d0f066098ac3e61f935faaa717d5
BenILBOUDO <102443261+BenILBOUDO@users.noreply.github.com>
2022-04-28 23:25:57 +0200

    Update fraude.py

--- fraude.py
+++ fraude.py
@@ -106,8 +106,8 @@ predict_btn = st.button("Identify", disabled=(uploaded_file is None))
 
 if predict_btn :
     Path_class = "classe_name.txt"
-    mod = "Libellé.h5"
-    print(mod)
+    #mod = "Libellé.h5"
+    #print(mod)
     model = load_model("Libellé.h5")
     model.summary()
     #image= [np.array(Image.open(path)
