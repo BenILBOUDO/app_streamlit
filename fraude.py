f5f9007762fae37d0c2db58f8aebfae46f61cedd
BenILBOUDO <102443261+BenILBOUDO@users.noreply.github.com>
2022-04-28 22:42:41 +0200

    Update fraude.py

--- fraude.py
+++ fraude.py
@@ -7,7 +7,7 @@ import tensorflow_hub as hub
 
 from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.models import Sequential
 
 from tensorflow_hub.keras_layer import KerasLayer
 
@@ -105,8 +105,8 @@ if uploaded_file:
 predict_btn = st.button("Identify", disabled=(uploaded_file is None))
 
 if predict_btn :
-    Path_class = "classe_name.txt"
-    model = load_model("Libellé.h5")
+    Path_class = "/app/app_streamlit/classe_name.txt"
+    model = load_model("/app/app_streamlit/Libellé.h5")
     model.summary()
     #image= [np.array(Image.open(path)
     #print(loaded_image.shape)
